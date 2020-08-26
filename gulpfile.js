b8d4f6ce1bb44f38dfbff66e1b7983a39a5414b7
Krikotik <ttv.webdev@gmail.com>
2020-08-26 12:57:03 +0300

    Добавление проекта в репозиторий
    Adding a project to the repository

---
+++ gulpfile.js
@@ -0,0 +1,104 @@
+let gulp = require('gulp'), // Ну это понятно
+    sass = require('gulp-sass'), // Это SASS
+    browserSync = require('browser-sync'), // BrowserSync
+    uglify = require('gulp-uglify'), // Минификация JS
+    concat = require('gulp-concat'), // Конкатинация
+    rename = require('gulp-rename'), // Ренейминг файлов
+    del = require('del'), // Удаление чего-либо
+    autoprefixer = require('gulp-autoprefixer'); // Добавление префиксов
+
+
+
+gulp.task('browser-sync', function() { // Функция подключения BrowserSync
+  browserSync.init({ // Я хуй знает, что это, но, по-моему, настройки этого дерьма
+      server: {
+          baseDir: ["app/", "app/views"] // Директория работы этой хуйни
+      }
+  });
+});
+
+// Функция подключения BrowserSync к HTML файлам
+gulp.task('html', function(){
+  return gulp.src('app/views/**/*.html') // Хде эти файлы
+  .pipe(browserSync.reload({stream: true})) // Само подключение BrowserSync
+});
+
+gulp.task('js', function(){ // Функция подключения BrowserSync к JS файлам
+  return gulp.src('app/js/*.js') // Хде эти файлы
+  .pipe(browserSync.reload({stream: true})) // Само подключение BrowserSync
+});
+
+
+// Функция компилинга SCSS в CSS
+gulp.task('sass', function(){
+  return gulp.src('app/scss/**/*.scss') // Откуда брать SCSS
+    .pipe(sass({outputStyle: 'compressed'})) // Сжатие CSS файла
+    .pipe(autoprefixer({ // Автопрефиксер
+      overrideBrowserslist: ['last 15 versions', '> 1%', 'ie 8', 'ie 7'],
+      cascade: false
+    }))
+    .pipe(rename({suffix: '.min'})) // Добавление суффикса ".min" для сжатого CSS файла
+    .pipe(gulp.dest('app/css')) // Куда суем скомпилированные файлы
+    .pipe(browserSync.reload({stream: true})) // Подключение BrowserSync для CSS
+});
+
+
+// Функция конкатинации библиотек CSS
+gulp.task('css-concat', function(){
+  return gulp.src([ // Откуда берем файлы для конкатинации
+    'node_modules/normalize.css/normalize.css',
+    'node_modules/slick-carousel/slick/slick.css'
+  ])
+    .pipe(concat('_libs.scss')) // В какой файл складываем сконкатинированные библиотеки
+    .pipe(gulp.dest('app/scss')) // Куды этот файл суем
+    .pipe(browserSync.reload({stream: true})) // Подключение BrowserSync
+});
+
+gulp.task('js-concat', function(){ // Функция конкатинации библиотек JS
+  return gulp.src([ // Откуда берем файлы для конкатинации
+    'node_modules/slick-carousel/slick/slick.js'
+  ])
+    .pipe(concat('libs.min.js')) // В какой файл складываем сконкатинированные библиотеки
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js')) // Куды этот файл суем
+    .pipe(browserSync.reload({stream: true})) // Подключение BrowserSync
+});
+
+
+// Функция удаления папки dist
+gulp.task('clean', async function(){
+  del.sync('dist')
+})
+
+// Экспортирование проекта в папку dist
+gulp.task('export', async function(){
+  let buildHtml = gulp.src('app/views/**/*.html') // Экспортирование HTML файлов
+    .pipe(gulp.dest('dist/views'));
+
+  let BuildCss = gulp.src('app/css/**/*.css') // Экспортирование CSS файлов
+    .pipe(gulp.dest('dist/css'));
+
+  let BuildJs = gulp.src('app/js/**/*.js') // Экспортирование JS файлов
+    .pipe(gulp.dest('dist/js'));
+
+  let BuildFonts = gulp.src('app/fonts/**/*.*') // Экспортирование шрифтов
+    .pipe(gulp.dest('dist/fonts'));
+
+  let BuildImg = gulp.src('app/img/**/*.*') // Экспортирование картинок
+    .pipe(gulp.dest('dist/img'));
+});
+
+
+// Функция Watch для слежения за изменениями файла
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('sass')); // Слежение за SCSS файлами
+  gulp.watch('app/views/**/*.html', gulp.parallel('html')) // Слежение за HTML файлами
+  gulp.watch('app/js/*.js', gulp.parallel('js')) // Слежение за JS файлами
+});
+
+
+// Запуск рабочего процесса GULP командой "gulp". Выполняются все указанные функции, и проект робит)
+gulp.task('default', gulp.parallel('css-concat' ,'sass', 'js-concat', 'browser-sync', 'watch'));
+
+// Функция билдинга проекта в папку dist
+gulp.task('build', gulp.series('clean', 'export'))
